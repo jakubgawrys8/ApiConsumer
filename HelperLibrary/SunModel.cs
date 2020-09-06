db427f5782c46d86be2859a46a4d10d99c3a9dd9
Jakub Gawrys <jakubgawrys8@gmail.com>
2020-09-06 15:41:27 +0200

    API implementation

---
+++ HelperLibrary/SunModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelperLibrary
+{
+    public class SunModel
+    {
+        public DateTime Sunrise { get; set; }
+        public DateTime Sunset { get; set; }
+    }
+}
---
+++ HelperLibrary/SunProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperLibrary
+{
+    public class SunProcessor
+    {
+        public static async Task<SunModel> LoadSunInformation()
+        {
+            string url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    SunResultModel result = await response.Content.ReadAsAsync<SunResultModel>();
+
+                    return result.Results;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
