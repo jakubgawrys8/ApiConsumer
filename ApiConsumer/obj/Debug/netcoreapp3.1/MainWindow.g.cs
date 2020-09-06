8c71576b952c35c7db1f12d2314bdb6ac7331a7e
Jakub Gawrys <jakubgawrys8@gmail.com>
2020-09-06 16:30:45 +0200

    Fixed assembly exceptions

--- ApiConsumer/obj/Debug/netcoreapp3.1/MainWindow.g.cs
+++ ApiConsumer/obj/Debug/netcoreapp3.1/MainWindow.g.cs
@@ -85,7 +85,7 @@ namespace ApiConsumer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ApiConsumer_hdnmfutm_wpftmp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ApiConsumer;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
